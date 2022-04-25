617cb796c07709597653f6ed3fabe3bacb116c8e
Evgeny <55916876+creepykedi@users.noreply.github.com>
2022-04-25 23:12:31 +0300

    remove pycache

--- db/db.py
+++ db/db.py
@@ -1,6 +1,9 @@
+import os
+
 from sqlmodel import create_engine
 from sqlmodel import Session
-eng = r'\Users\Evgeny\PycharmProjects\jewels\database.db'
+eng = os.curdir.join('database.db')
+
 sqlite_url = f'sqlite:///{eng}'
 engine = create_engine(sqlite_url, echo=True)
 session = Session(bind=engine)
