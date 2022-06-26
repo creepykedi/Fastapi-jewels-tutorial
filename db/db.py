2bfabe4a26c8945e521f7335aa2e6b0ef076fb24
Evgeny <55916876+creepykedi@users.noreply.github.com>
2022-06-26 22:52:27 +0300

    bug fix

--- db/db.py
+++ db/db.py
@@ -1,9 +1,8 @@
-import os
-
 from sqlmodel import create_engine
 from sqlmodel import Session
-eng = os.curdir.join('database.db')
+eng = 'database.db'
 
 sqlite_url = f'sqlite:///{eng}'
 engine = create_engine(sqlite_url, echo=True)
 session = Session(bind=engine)
+print(sqlite_url)
